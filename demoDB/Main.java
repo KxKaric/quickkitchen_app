9c33490ea4277e944269ebacbcb06230c18a6391
Jayed Rafi <rafijs@myumanitoba.ca>
2023-02-04 21:42:15 +0000

    Upload New File

---
+++ demoDB/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        RecipeDatabase obj = new RecipeDatabase();
+        ArrayList<Recipe> show = obj.getDatabase();
+
+    }}
