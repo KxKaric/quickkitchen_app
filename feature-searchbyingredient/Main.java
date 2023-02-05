b375fb95a4dd12584ec22208cfff7bacd5818632
Jayed Rafi <rafijs@myumanitoba.ca>
2023-02-05 04:53:25 +0000

    Upload New File

---
+++ feature-searchbyingredient/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        //RecipeDatabase obj = new RecipeDatabase();
+        //ArrayList<Recipe> show = obj.getDatabase();
+
+        IngredientSearch x = new IngredientSearch("Cheese");
+        x.showList();
+    }}
