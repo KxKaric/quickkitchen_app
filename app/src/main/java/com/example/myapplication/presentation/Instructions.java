ff6dbd850d28080d361e92ed17a92dc909237e53
Omar Moustafa <moustaor@myumanitoba.ca>
2023-02-06 03:21:50 +0000

    test

--- app/src/main/java/com/example/myapplication/presentation/Instructions.java
+++ app/src/main/java/com/example/myapplication/presentation/Instructions.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import com.example.myapplication.R;
 
 public class Instructions extends AppCompatActivity {
-
+    //Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
