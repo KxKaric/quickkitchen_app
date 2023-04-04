65eb4ee45729a3aa21bb45ffd9550cec418cb0d3
moustaor@myumanitoba.ca <Ashgan99>
2023-04-04 00:46:01 -0500

    igrediendt test system search

--- app/src/androidTest/java/comp3350/quickkitchen/IngredientSearchSystemTest.java
+++ app/src/androidTest/java/comp3350/quickkitchen/IngredientSearchSystemTest.java
@@ -34,52 +34,14 @@ import static org.junit.Assert.*;
 import comp3350.quickkitchen.presentation.BookMarkActivity;
 import comp3350.quickkitchen.presentation.HomeActivity;
 import static org.hamcrest.CoreMatchers.anything;
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 @RunWith(AndroidJUnit4.class)
-public class BookMarkSystemTest {
+public class IngredientSearchSystemTest {
     /**
      * System testing
      */
     @Rule
     public ActivityScenarioRule<HomeActivity> activityRule = new ActivityScenarioRule<>(HomeActivity.class);
-
-
-    @Test
-    public void shoppingListSystemTest() {
-        // -- Home page --
-        onView(withId(R.id.oilBtn)).perform(click());
-        onView(withId(R.id.searchBtn)).perform(click());
-
-        // -- Recipe page --
-        onData(anything()).inAdapterView(withId(R.id.recipeList)).atPosition(0).perform(click());
-        pressBack(); //closing the popup
-        onView(withId(R.id.nextBtn)).perform(click());
-
-        // -- Instructions page --
-        onView(withId(R.id.ShoppingList)).perform(click());
-
-        // -- Shopping List page --
-        onView(withId(R.id.shopping_list)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void showStepsSystemTest() {
-        // -- Home page --
-        onView(withId(R.id.oilBtn)).perform(click());
-        onView(withId(R.id.searchBtn)).perform(click());
-
-        // -- Recipe page --
-        onData(anything()).inAdapterView(withId(R.id.recipeList)).atPosition(0).perform(click());
-        pressBack(); //closing the popup
-        onView(withId(R.id.nextBtn)).perform(click());
-
-        // -- Instructions page --
-        onView(withId(R.id.instrutions_list)).check(matches(isDisplayed()));
-    }
 
     @Test
     public void ingredientsSearchSystemTest() {
@@ -91,23 +53,5 @@ public class BookMarkSystemTest {
         // -- Recipe page --
         onView(withId(R.id.recipeList)).check(matches(isDisplayed()));
     }
-
-    @Test
-    public void bookMarkingSystemTest() {
-        // -- Home page --
-        onView(withId(R.id.oilBtn)).perform(click());
-        onView(withId(R.id.searchBtn)).perform(click());
-
-        // -- Recipe page --
-        onData(anything()).inAdapterView(withId(R.id.recipeList)).atPosition(0).perform(longClick());
-//        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-//        onView(withId(R.id.bookMarkBtn)).check(matches(isDisplayed()));
-//        onView(withId(R.id.add_to_bookmark)).perform(click());
-//        pressBack(); //closing the popup
-
-        // -- Home page --
-//        onView(withId(R.id.bookMarkBtn)).perform(click());
-    }
-
-
 }
+
---
+++ app/src/androidTest/java/comp3350/quickkitchen/ShowStepsSystemTest.java
@@ -0,0 +1,4 @@
+package comp3350.quickkitchen;
+
+public class ShowStepsSystemTest {
+}
