3a6158a28b5f6623a301aa18cfa6570c5c33087c
Jayed Rafi <jayedrafi@ieee.org>
2023-04-05 21:02:42 -0500

    Update IngredientSearch.java

--- app/src/main/java/comp3350/quickkitchen/features/IngredientSearch.java
+++ app/src/main/java/comp3350/quickkitchen/features/IngredientSearch.java
@@ -44,7 +44,6 @@ public class IngredientSearch {
          * Search db and return a list of Recipe that use the given ingredients
          */
         //sorted list based on rank
-        //this.recipeList=recipePersistence.sortByRank(recipePersistence.getRecipeByIngredient(ingredients));
         this.recipeList = rankObj.sortByRank(recipePersistence.getRecipeByIngredient(ingredients));
         // rank method here
         return this.recipeList;
